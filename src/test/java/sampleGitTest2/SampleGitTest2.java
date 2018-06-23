e6c90f5ead118250577d4b29c24181173948852c
unknown <parthibansudhan@gmail.com>
2018-06-23 14:31:17 +0530

    New File

---
+++ src/test/java/sampleGitTest2/SampleGitTest2.java
@@ -0,0 +1,29 @@
+package sampleGitTest2;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleGitTest2 {
+	
+	@Test
+	public void loginTest() throws IOException {
+
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("http://www.ntltaxi.com/");
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\Admin\\eclipse-workspace\\sampleGitTest\\screenshot\\test1.jpg"));
+		
+	}
+	
+	
+}
